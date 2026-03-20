40e79df0f6e7be5c03cd0c31cde42a25abbac870
abdulaziz <telegrampreimum4@gmail.com>
2026-03-20 21:54:41 +0500

    #test

--- .history/src/App_20260320214316.jsx
+++ .history/src/App_20260320214316.jsx
@@ -355,7 +355,7 @@ export default function App() {
               value={query}
               onChange={(event) => setQuery(event.target.value)}
               placeholder="Qidirish..."
-              className="h-7 border-0 !bg-transparent px-0 text-[15px] text-slate-900 placeholder:text-slate-500 focus-visible:ring-0 flex-1"
+              className="h-7 border-0 bg-transparent px-0 text-[15px] text-slate-900 placeholder:text-slate-500 focus-visible:ring-0 flex-1"
             />
             {query && (
               <button
